19beb895a27f73e64174b77c7726315779e925b9
operat0r <freeload101@users.noreply.github.com>
2021-02-27 17:49:10 -0500

    Delete DOMAIN2IP.sh

--- DOMAIN2IP.sh
+++
@@ -1,3 +0,0 @@
-lynx -width=999 -nolist -dump "https://crt.sh/?q=%25${1}%25" | awk '{gsub(" ","\n"); print}' | grep '\.' | sed 's/\*\.//g' | sort -u > tmp
-nmap -n -T5 -sP -iL tmp -oA tmp
-cat tmp.gnmap | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort -u
