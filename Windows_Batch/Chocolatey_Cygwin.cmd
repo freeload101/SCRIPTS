316e91032f6438d41e32a2512bd5802ade16f062
operat0r <freeload101@users.noreply.github.com>
2021-04-19 09:34:40 -0400

    Update Chocolatey_Cygwin.cmd

--- Windows_Batch/Chocolatey_Cygwin.cmd
+++ Windows_Batch/Chocolatey_Cygwin.cmd
@@ -6,7 +6,12 @@ echo ###########################################################################
 
 SET DIR=%~dp0%
 
-timeout /t 5
+timeout /t 5 >> %temp%\null
+
+
+ 
+
+
 echo [+] Checking powershell version...
 @powershell if ($PSVersionTable.PSVersion.Major -eq 5) {    Write-Host " [+] You are running PowerShell version 5"}else {    Write-Host " [+] This is version $PSVersionTable.PSVersion.Major Please update!!!";Start-Sleep -s 99 }
 
@@ -14,6 +19,8 @@ echo [+] Checking powershell version...
 echo [+] Checking for admin ...
 FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
 IF (%adminTest%)==(Access) goto noAdmin
+
+goto main1
 
 :noAdmin
 echo [+] You must run this script as an Administrator!
@@ -23,7 +30,23 @@ exit
 :theEnd
 
 
+:main1
+echo [+] Disabling PowerShell Executionpolicy
+@powershell.exe   -Enc UwBlAHQALQBFAHgAZQBjAHUAdABpAG8AbgBQAG8AbABpAGMAeQAgAC0ARQB4AGUAYwB1AHQAaQBvAG4AUABvAGwAaQBjAHkAIABVAG4AcgBlAHMAdAByAGkAYwB0AGUAZAAgAC0ARgBvAHIAYwBlAA==
+ 
+:::: https://raw.githubusercontent.com/rkovar/PowerShell/master/audit.bat
 
+ 
+::
+::  These settings will only change the local security policy.  It is best to set these in Group Policy default profile so all systems get the same settings.  
+::  GPO will overwrite these settings!
+::
+::  Be sure to set "Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings." in the: Local Security Policy - Local Policies - Security Options
+::  Or the Advanced settings will NOT apply.  
+::  Also EVERY setting must have the "Configure the following events" box checked or all the settings will NOT apply ( I consider this a bug)
+::
+::#######################################################################
+::
 
 
 echo [+] Downloading/Installing RemoveW10Bloat.bat
@@ -67,3 +90,9 @@ echo [+] Downloading/Installing Cygwin Portable
 cd "%DIR%"
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/vegardit/cygwin-portable-installer/master/cygwin-portable-installer.cmd','%DIR%cygwin-portable-installer.cmd'))"
 cmd /c cygwin-portable-installer.cmd
+
+:theEnd
+
+echo [+] All done!
+pause
+exit
