2f260bbdbe77b192117f19a3ea3197ee93e3dda4
operat0r <freeload101@users.noreply.github.com>
2021-09-07 09:06:52 -0400

    Update BleachBit_Quickkill.bat

--- Windows_Batch/BleachBit_Quickkill.bat
+++ Windows_Batch/BleachBit_Quickkill.bat
@@ -14,6 +14,9 @@ CALL :INIT
 CALL :QUICKKILL
 CALL :CATCH
 
+CALL :DELALLUSERS
+CALL :CATCH
+
 CALL :RUNCLEANMGR
 CALL :CATCH
 
@@ -65,6 +68,17 @@ FOR /F "tokens=* delims=" %%A in ('reg QUERY "HKLM\SOFTWARE\Microsoft\Windows\Cu
 REG ADD "%%A"  /v StateFlags0777 /t REG_DWORD /d 00000002 /f
 )
 cleanmgr /sagerun:777
+EXIT /B %ERRORLEVEL%
+
+
+:DELALLUSERS
+echo %date% %time% INFO: Removing All users temp files
+FOR /F "delims==" %%A IN ('DIR/B "C:\Users"') DO rd /s/q "C:\Users\%%A\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5\"
+FOR /F "delims==" %%A IN ('DIR/B "C:\Users"') DO rd /s/q "C:\Users\%%A\AppData\Local\History\"
+FOR /F "delims==" %%A IN ('DIR/B "C:\Users"') DO rd /s/q "C:\Users\%%A\AppData\Local\Temp\"
+FOR /F "delims==" %%A IN ('DIR/B "C:\Users"') DO rd /s/q "C:\Users\%%A\AppData\Roaming\Microsoft\Windows\Cookies\"
+FOR /F "delims==" %%A IN ('DIR/B "C:\Users"') DO rd /s/q "C:\Users\%%A\AppData\Roaming\Microsoft\Windows\Recent\"
+FOR /F "delims==" %%A IN ('DIR/B "C:\Users"') DO rd /s/q "C:\Users\%%A\Local Settings\Temporary Internet Files\"
 EXIT /B %ERRORLEVEL%
 
 
