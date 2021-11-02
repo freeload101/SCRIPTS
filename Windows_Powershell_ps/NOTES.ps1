807c897b1f146d6c4a3c6142e5293b6c5bf67481
operat0r <freeload101@users.noreply.github.com>
2021-11-02 15:21:42 -0400

    BitLocker Remote Lock
    BitLocker Remote Lock

--- Windows_Powershell_ps/NOTES.ps1
+++ Windows_Powershell_ps/NOTES.ps1
@@ -115,4 +115,26 @@ start .\emails.csv
 
 
 
+###################################################### 
+
+## Wipe existing BitLocker protections
+manage-bde -protectors -delete C:
+# Create new, randomly generated recovery password 
+manage-bde -protectors -add C: -RecoveryPassword
+# Verify new recovery password will be required on next reboot
+manage-bde -protectors -enable C:
+# Force the user to be prompted for new recovery password
+manage-bde -forcerecovery C:
+
+#############################################################
+####################### WARNING #############################
+#############################################################
+# YOU MUST COPY THE KEY (PASSWORD) TO UNLOCK THE DRIVE IF YOU LOSE THE KEY YOU WILL NOT BE ABLE TO RECOVER ANYTHING FROM THE C: DRIVE !!!
+# EXAMPLE 713438-591129-666237-608498-028864-058685-409024-701756
+
+
+
+
+# force Reboot system to trigger recovery prompt 
+Restart-Computer -Force
 
