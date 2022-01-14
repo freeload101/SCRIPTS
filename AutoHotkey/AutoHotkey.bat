de568a6997eb59489f00df923828425ccae6daf9
operat0r <freeload101@users.noreply.github.com>
2022-01-14 13:45:15 -0500

    Update AutoHotkey.bat

--- AutoHotkey/AutoHotkey.bat
+++ AutoHotkey/AutoHotkey.bat
@@ -1,9 +1,9 @@
 @echo off
-setlocal enabledelayedexpansion
+Setlocal EnableDelayedExpansion EnableExtensions
 
 echo '-----------------------------------------------------------------------------------------'
 echo 'rmccurdy.com AutoHotkey Installer'
-echo 'ver 1.0a'
+echo 'ver 1.2a'
 echo '-----------------------------------------------------------------------------------------'
 
 CALL :INIT
@@ -16,38 +16,72 @@ CALL :CATCH
 CALL :END
 
 
+
 :CHECK
-if not exist "%~dp0wget.exe" (
+if exist "%~dp0wget.exe" (
+ 	set "WGETPATH=%~dp0wget.exe"
+ 	)
+if exist "Ac:\windows\system32\wget.exe" (
+	set "WGETPATH=c:\windows\system32\wget.exe"
+	)	
+
+if not "!WGETPATH!"=="" (
+    echo %date% %time% INFO: wget.exe found !WGETPATH!
+) else (
 	echo %date% %time% INFO: No wget.exe found
 	CALL :DLWGET
-	)
+	set "WGETPATH=%~dp0wget.exe"
+)
+
+
+
+
 if not exist "%~dp0AHK\AutoHotkey.exe" (
 	echo %date% %time% INFO: No AutoHotkey found
 	CALL :DLAHK
-		)
+	)
 if not exist "%~dp0C0ffee Anti Idle.ahk" (
-	echo %date% %time% INFO: AutoHotkey script not found
+	echo %date% %time% INFO: AutoHotkey script "%~dp0C0ffee Anti Idle.ahk" not found downloading 
 	cd "%~dp0"
-	"%~dp0wget.exe"  "https://raw.githubusercontent.com/freeload101/SCRIPTS/master/AutoHotkey/C0ffee Anti Idle.ahk" )
-
-if exist "%~dp0C0ffee Anti Idle.ahk" (
-	echo %date% %time% INFO: Starting AutoHotkey
-start ""	"%~dp0AHK\AutoHotkey.exe" "%~dp0C0ffee Anti Idle.ahk"
+	"%WGETPATH%" -q "https://raw.githubusercontent.com/freeload101/SCRIPTS/master/AutoHotkey/C0ffee Anti Idle.ahk" 
 	)
 
+if exist "%~dp0C0ffee Anti Idle.ahk" (
+	echo %date% %time% INFO: Starting AutoHotkey with "%~dp0C0ffee Anti Idle.ahk"
+start ""	"%~dp0AHK\AutoHotkey.exe" "%~dp0C0ffee Anti Idle.ahk"
+	)
 EXIT /B %ERRORLEVEL%
 
+
+
+
+REM Don't need 7zip if I can bypass admin prompt with compat layer set
+:DL7IP
+(
+echo %date% %time% INFO: Downloading latest 7zip
+mkdir "%~dp07ZIP_BASE"
+wget  "https://sourceforge.net/projects/sevenzip/files/latest/download"  -A *.exe -O 7zip.exe
+move /Y 7zip.exe "%~dp07ZIP_BASE"
+cd %~dp07ZIP_BASE
+echo %date% %time% INFO: Installing 7zip
+set __COMPAT_LAYER=RUNASINVOKER 
+"%~dp07ZIP_BASE\7zip.exe /S /D="%~dp07ZIP"
+
+)
+
+
+EXIT /B %ERRORLEVEL%
 
 
 :DLAHK
 (
 echo %date% %time% INFO: Downloading latest AutoHotkey
-wget -U "rmccurdy.com"  -P AHK_BASE  -e robots=off  -nd -r  "https://github.com/Lexikos/AutoHotkey_L/releases/latest" --max-redirect 1 -l 1 -A "latest,*.exe" -R '*.gz,release*.*' --regex-type pcre --accept-regex ".*\.exe"
+wget -q -U "rmccurdy.com"  -P AHK_BASE  -e robots=off  -nd -r  "https://github.com/Lexikos/AutoHotkey_L/releases/latest" --max-redirect 1 -l 1 -A "latest,*.exe" -R '*.gz,release*.*' --regex-type pcre --accept-regex ".*\.exe"
 cd "%~dp0AHK_BASE"
 rename "*.exe" "AutoHotkey.exe" 
 echo %date% %time% INFO: Installing AutoHotkey
-"%~dp0AHK_BASE\AutoHotkey.exe" /S /D="%~dp0AHK"
-CALL :END
+set __COMPAT_LAYER=RUNASINVOKER 
+"%~dp0AHK_BASE\AutoHotkey.exe"  /S /D="%~dp0AHK"
 )
 EXIT /B %ERRORLEVEL%
 
@@ -74,8 +108,9 @@ cd "%~dp0"
 taskkill /F /IM AutoHotkey.exe 2>> "log.txt"
 rd /q/s "%~dp0AHK_BASE" 2>> "log.txt"
 ) 
-EXIT /B %ERRORLEVEL%
+EXIT /B 0
 
 :END
 echo %date% %time% INFO: All done
+pause
 exit
