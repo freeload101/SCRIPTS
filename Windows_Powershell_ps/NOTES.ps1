a64a66415a13d077b45778894af82819e5dce28f
operat0r <freeload101@users.noreply.github.com>
2022-01-25 19:44:57 -0500

    Update NOTES.ps1

--- Windows_Powershell_ps/NOTES.ps1
+++ Windows_Powershell_ps/NOTES.ps1
@@ -183,3 +183,7 @@ Import-Module ActiveDirectory
 Search-ADAccount –SearchBase ‘OU=YOUROUNAMEHERE,DC=YOURDOMAINHERE,DC=com’ –LockedOut | Unlock-ADAccount -Passthru
 
 
+
+# password not required UF_PASSWD_NOTREQD
+Get-ADUser -Filter {PasswordNotRequired -eq $true} |  Export-Csv -Path .\PasswordNotRequired.csv
+start .\PasswordNotRequired.csv
