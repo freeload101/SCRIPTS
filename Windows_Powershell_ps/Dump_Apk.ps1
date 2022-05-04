274f07a8b6dff87bf7474ddad6c0506e0653e68e
Robert McCurdy <freeload101@users.noreply.github.com>
2022-05-04 14:44:59 -0400

    Add files via upload

---
+++ Windows_Powershell_ps/Dump_Apk.ps1
@@ -0,0 +1,18 @@
+﻿#cd C:\DELETE\_ANDROID\platform-tools
+#$Env:PATH += ";C:\DELETE\_ANDROID\platform-tools"
+adb shell pm list packages > apklist.txt
+Start-Sleep -s 5
+$apks = ((Get-Content .\apklist.txt)) -replace 'package:',''
+ ForEach ($apk in $apks) {
+    echo "APK is $apk"
+    md $apk
+    # If in secondary profile, add "--user 15" after path, before $file
+    adb shell pm path $apk
+    #Start-Sleep -s 1
+    $filepath = ((adb shell pm path $apk | % {$_.replace("package:","")}))
+    ForEach ($lapk in $filepath | % {$_.replace("package:","")}) {
+        echo "pulling $lapk $apk"
+        adb pull $lapk $apk
+        #Start-Sleep -s 1
+    }
+ }
