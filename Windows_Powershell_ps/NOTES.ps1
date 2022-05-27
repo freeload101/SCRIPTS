1241f62c77d7abed502a40cd2d5c4e03106b6778
Robert McCurdy <freeload101@users.noreply.github.com>
2022-05-27 09:41:01 -0400

    Update NOTES.ps1

--- Windows_Powershell_ps/NOTES.ps1
+++ Windows_Powershell_ps/NOTES.ps1
@@ -203,3 +203,9 @@ start .\ADAudit.csv
 $WScript.CreateShortcut($_.FullName).Arguments}| sort -Unique | Select-String -Pattern 'WINDOWS|Teams|program files' -NotMatch 
 
 
+
+# Get CPU usage for top 5 process
+echo "Computer Name: $env:COMPUTERNAME"
+Get-Process | Sort CPU -descending | Select -first 5 -Property ID, ProcessName, Description, CPU 
+
+        
