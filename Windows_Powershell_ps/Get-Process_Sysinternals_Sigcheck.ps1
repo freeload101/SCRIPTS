b851e39961e625b13f905f0e942aa420e1d742b3
Robert McCurdy <freeload101@users.noreply.github.com>
2022-07-31 00:16:15 -0400

    Update Get-Process_Sysinternals_Sigcheck.ps1

--- Windows_Powershell_ps/Get-Process_Sysinternals_Sigcheck.ps1
+++ Windows_Powershell_ps/Get-Process_Sysinternals_Sigcheck.ps1
@@ -1,4 +1,4 @@
-(New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/Sigcheck.zip', 'Sigcheck.zip')
-Expand-Archive .\Sigcheck.zip -DestinationPath .\ -Force
+(New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/Sigcheck.zip', 'c:\windows\temp\Sigcheck.zip')
+Expand-Archive c:\windows\temp\Sigcheck.zip -DestinationPath c:\windows\temp\ -Force
 reg add "HKCU\Software\Sysinternals\SigCheck\VirusTotal" /v "VirusTotalTermsAccepted" /t REG_DWORD /d 1 /f
-(Get-Process).Path| sort -Unique|ForEach-Object { sigcheck64 -vt -nobanner -accepteula -c "$_" | Select-String -Pattern 'Signed|VT link' -NotMatch }
+(Get-Process).Path| sort -Unique|ForEach-Object { c:\windows\temp\sigcheck64 -vt -nobanner -accepteula -c "$_" | Select-String -Pattern 'Signed|VT link' -NotMatch }
