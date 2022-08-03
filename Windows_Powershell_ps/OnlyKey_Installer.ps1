37495fb9f7e2df3679a4d167c2d2574a4b334680
Robert McCurdy <freeload101@users.noreply.github.com>
2022-08-03 00:33:17 -0400

    Create OnlyKey_Installer.ps1

---
+++ Windows_Powershell_ps/OnlyKey_Installer.ps1
@@ -0,0 +1,11 @@
+$downloadUri = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/trustcrypto/OnlyKey-App/releases/latest").assets | Where-Object name -like *.exe ).browser_download_url
+Invoke-WebRequest -Uri $downloadUri -Out $env:LOCALAPPDATA\Onlykey.exe
+$Env:__COMPAT_LAYER='RunAsInvoker'
+Start-Process -FilePath "$env:LOCALAPPDATA\Onlykey.exe" -ArgumentList  " /D=$env:LOCALAPPDATA\Onlykey\  "  #-Wait  -Verbose -WindowStyle Hidden 
+
+start-sleep -Seconds 2
+$SendWait = New-Object -ComObject wscript.shell;
+$SendWait.SendKeys('{ENTER}')
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
