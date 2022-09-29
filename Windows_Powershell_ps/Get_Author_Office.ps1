ced00cdce1bf8838b16fe94f9e4ea14415c9dba0
Robert McCurdy <freeload101@users.noreply.github.com>
2022-09-29 10:19:20 -0400

    Create Get_Author_Office.ps1

---
+++ Windows_Powershell_ps/Get_Author_Office.ps1
@@ -0,0 +1,22 @@
+(Get-ChildItem -Path "\\DEEERPPP\Everyone" -Depth 1 -Filter *.xlsx) |ForEach-Object { 
+
+$FullName = $_.FullName
+
+$Folder = Split-Path $FullName
+$File = Split-Path $FullName -Leaf
+
+$objShell = New-Object -ComObject Shell.Application
+$objFolder = $objShell.namespace($Folder)
+ 
+
+$Item = $objFolder.items().item($File)
+$Author = $objFolder.getDetailsOf($Item, 20)
+
+#Write-Host "$FullName,`"$Author`""
+
+$Author = $Author -replace '""',''
+
+Write-Output "$Author;" 
+
+} | sort -Unique |  Out-File -FilePath Author.txt
+start Author.txt
