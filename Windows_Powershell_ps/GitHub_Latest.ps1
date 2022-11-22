a1ec3a04c1521702dc71814c37739f14ff3bf648
Robert McCurdy <freeload101@users.noreply.github.com>
2022-11-22 09:15:34 -0500

    Update GitHub_Latest.ps1

--- Windows_Powershell_ps/GitHub_Latest.ps1
+++ Windows_Powershell_ps/GitHub_Latest.ps1
@@ -2,7 +2,7 @@
 
 $downloadUri = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/trustcrypto/OnlyKey-App/releases/latest").assets | Where-Object name -like *.exe ).browser_download_url
 echo $downloadUri
-#Invo1ke-WebRequest -Uri $downloadUri -Out $env:TEMP\Onlykey.zip
+#Invoke-WebRequest -Uri $downloadUri -Out $env:TEMP\Onlykey.zip
 #Expand-Archive -Path $env:TEMP\Onlykey.zip -DestinationPath "$env:TEMP\" -Force
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 Add-Type -AssemblyName System.IO.Compression
