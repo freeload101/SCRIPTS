8c6f3bd888d90da70c479f8d531757e91747b050
Robert McCurdy <freeload101@users.noreply.github.com>
2022-12-17 16:49:24 -0500

    Something like that ...

---
+++ Bash/Plex to Traktor.sh
@@ -0,0 +1,27 @@
+# rename Plex offline to Traktor import ...
+# C:\Users\internet\AppData\Local\Plexamp\Plexamp\Offline
+#for i in `find . -type f \( ! -iname "*.flac" ! -iname "*.mp3" ! -iname "*.ogg"  \) `
+for i in `find . `
+do
+export FileName="$i"
+export FileType=`file "$FileName"`
+echo $i
+echo $FileType
+
+	if [[ "$FileType"  == *MPEG*  ]]
+	then
+		mv "$FileName" "${FileName}.mp3"
+	fi
+	
+	if [[ "$FileType"  == *FLAC*  ]]
+	then
+		mv "$FileName" "${FileName}.flac"
+	fi
+	if [[ "$FileType"  == *Ogg*  ]]
+	then
+		mv "$FileName" "${FileName}.ogg"
+	fi
+
+	
+done
+
