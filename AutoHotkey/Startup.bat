52fac29be1c2420e6b5f370c51df4d64068931ed
Robert McCurdy <freeload101@users.noreply.github.com>
2023-02-24 17:41:13 -0500

    Add files via upload

---
+++ AutoHotkey/Startup.bat
@@ -0,0 +1,14 @@
+if not exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Startup.bat" (
+copy Startup.bat "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+)
+
+cd "%USERPROFILE%" 
+
+PowerShell -Command "Set-ExecutionPolicy Unrestricted"  
+
+::download install.ps1
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://github.com/freeload101/SCRIPTS/raw/master/AutoHotkey/AutoHotkey.ps1','%USERPROFILE%\AutoHotkey.ps1'))"
+::run installer
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%USERPROFILE%\AutoHotkey.ps1' %*"
+
+pause
