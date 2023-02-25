190a32a22db54d69370697f70eb1d59b910d0402
Robert McCurdy <freeload101@users.noreply.github.com>
2023-02-25 18:51:55 -0500

    Update Startup.bat

--- AutoHotkey/Startup.bat
+++ AutoHotkey/Startup.bat
@@ -10,7 +10,7 @@ PowerShell -Command "Set-ExecutionPolicy Unrestricted"
 if not exist "%USERPROFILE%\AutoHotkey.ps1" (
 ::download install.ps1
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://github.com/freeload101/SCRIPTS/raw/master/AutoHotkey/AutoHotkey.ps1','%USERPROFILE%\AutoHotkey.ps1'))"
-::run installer
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%USERPROFILE%\AutoHotkey.ps1' %*"
 
 )
+::run installer
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%USERPROFILE%\AutoHotkey.ps1' %*"
