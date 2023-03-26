addfcd81685cec130f3089da7915d7c45d462b9b
Robert McCurdy <freeload101@users.noreply.github.com>
2023-03-26 08:35:35 -0700

    Create UDM_Unifi_Block_Ads.sh

---
+++ Bash/UDM_Unifi_Block_Ads.sh
@@ -0,0 +1,13 @@
+# download python script 
+wget 'https://github.com/freeload101/Python/raw/master/UDM_Unifi_Block_Ads.py'
+chmod 777 UDM_Unifi_Block_Ads.py
+# dump conf
+./UDM_Unifi_Block_Ads.py > /etc/dnsmasq.d/dnsmasq.adblock.conf
+
+# update /etc/dnsmasq.conf
+sed -i 's/#conf-dir=\/etc\/dnsmasq.d\/,\*\.conf/conf-dir=\/etc\/dnsmasq.d\/,\*\.conf/g' /etc/dnsmasq.conf
+
+# reload dnsmasq
+/etc/init.d/dnsmasq force-reload
+
+# test with nslookup doubleclick.net
