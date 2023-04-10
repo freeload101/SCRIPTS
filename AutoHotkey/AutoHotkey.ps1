965809ffa0100d0d47b660c4e4772efcedfec647
Robert McCurdy <freeload101@users.noreply.github.com>
2023-04-10 09:14:49 -0400

    fixed-ish

--- AutoHotkey/AutoHotkey.ps1
+++ AutoHotkey/AutoHotkey.ps1
@@ -3,14 +3,7 @@ $VARCD = (Get-Location)
 Set-Location -Path "$env:userprofile"
 $VARCD = $env:userprofile
 Write-Host "[+] Current Working Directory $VARCD"
-
-<#
-# Download 7zip to extract 
-$downloadUri = ((Invoke-WebRequest -UseBasicParsing "https://www.7-zip.org/download.html").links | select  href   | Where-Object href -like 'a/7z*-x64.exe'  | select -first 1).href
-Invoke-WebRequest -Uri "https://www.7-zip.org/$downloadUri" -Out "$VARCD\7zip.exe" -Verbose
-Start-Process -FilePath "$VARCD\7zip.exe" -WorkingDirectory "$VARCD" -ArgumentList "  /S /D=`"$VARCD\7zip`" " 
-#>
-
+ 
 # run without admin !
 $env:__COMPAT_LAYER="RUNASINVOKER"
 
@@ -40,4 +33,4 @@ Write-Host "[+] Download latetst AHK script"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/freeload101/SCRIPTS/master/AutoHotkey/C0ffee Anti Idle v2.ahk" -OutFile  "$VARCD\C0ffee Anti Idle v2.ahk"
 
 # start AHK 
-Start-Process -FilePath "C:\Users\internet\autohotkey\AutoHotkey64.exe" -WorkingDirectory "$VARCD" -ArgumentList " `"$VARCD\C0ffee Anti Idle v2.ahk`"  " 
+Start-Process -FilePath "$VARCD\autohotkey\AutoHotkey64.exe" -WorkingDirectory "$VARCD" -ArgumentList " `"$VARCD\C0ffee Anti Idle v2.ahk`"  " 
