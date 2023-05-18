83a2c3724ef2137d8b787e8e05db101cf0df52bb
Robert McCurdy <freeload101@users.noreply.github.com>
2023-05-18 10:50:48 -0400

    Update PSQuickKill.ps1

--- Windows_Powershell_ps/PSQuickKill.ps1
+++ Windows_Powershell_ps/PSQuickKill.ps1
@@ -1,23 +1,15 @@
 Set-Variable -Name ErrorActionPreference -Value SilentlyContinue
 
-#Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell")} 
-
-#Set-Variable -Name ErrorActionPreference -Value SilentlyContinue
-# Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell")}|Stop-Process  -Force
-
-
-
-
 $count = (Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell")}).Count
 write-host "$count` Processes to be killed `n"
 
-#(Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell")}).ProcessName
-Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "powershell")}|Stop-Process  -Force
-
-
+Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "powershell" -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell_ise") -and !($_.ProcessName -eq "wdm") -and !($_.ProcessName -eq "conhost")  )}  | Sort-Object | Get-Unique | foreach { 
+write-host "About to kill " $_ 
+Stop-Process $_ -Force 
+} 
+ 
 $count = (Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell")}).Count
 write-host "$count` Processes Active `n"
-
 
 write-host "`DONE`n"
 Start-Sleep -Seconds 10
