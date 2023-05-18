bb31de8ec7b4d77188ecff48331d9c4fe7a59876
Robert McCurdy <freeload101@users.noreply.github.com>
2023-05-18 11:02:01 -0400

    Update PSQuickKill.ps1

--- Windows_Powershell_ps/PSQuickKill.ps1
+++ Windows_Powershell_ps/PSQuickKill.ps1
@@ -5,16 +5,12 @@ write-host "$count` Processes to be killed `n"
 
 Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "powershell" -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell_ise") -and !($_.ProcessName -eq "wdm") -and !($_.ProcessName -eq "conhost")  )}  | Sort-Object | Get-Unique | foreach { 
 write-host "About to kill " $_ 
-Stop-Process $_ -Force 
-} 
- 
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds .5
+Stop-Process -Name $_ -Force 
+Stop-Process -Name chrome
 
-Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "powershell" -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell_ise") -and !($_.ProcessName -eq "wdm") -and !($_.ProcessName -eq "conhost")  )}  | Sort-Object | Get-Unique | foreach { 
-write-host "About to kill " $_ 
-Stop-Process $_ -Force 
 } 
-
+  
 
 $count = (Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell")}).Count
 write-host "$count` Processes Active `n"
