ad83daecc4b3a352f33c51ee417bf756db90afe6
Robert McCurdy <freeload101@users.noreply.github.com>
2023-05-18 11:05:02 -0400

    Update PSQuickKill.ps1

--- Windows_Powershell_ps/PSQuickKill.ps1
+++ Windows_Powershell_ps/PSQuickKill.ps1
@@ -6,8 +6,8 @@ write-host "$count` Processes to be killed `n"
 Get-Process | Where{!($_.UserName -match "NT AUTHORITY\\(?:SYSTEM|(?:LOCAL|NETWORK) SERVICE)") -and !($_.ProcessName -eq "powershell" -and !($_.ProcessName -eq "explorer") -and !($_.ProcessName -eq "powershell_ise") -and !($_.ProcessName -eq "wdm") -and !($_.ProcessName -eq "conhost")  )}  | Sort-Object | Get-Unique | foreach { 
 write-host "About to kill " $_ 
 Start-Sleep -Seconds .5
-Stop-Process -Name $_ -Force 
-Stop-Process -Name chrome
+Stop-Process  $_ -Force 
+ 
 
 } 
   
