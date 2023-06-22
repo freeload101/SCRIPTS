56f6883521c40ac834452944e67418af34514e64
Robert McCurdy <freeload101@users.noreply.github.com>
2023-06-21 20:19:11 -0400

    Update Plex to Traktor.sh

--- Bash/Plex to Traktor.sh
+++ Bash/Plex to Traktor.sh
@@ -1,6 +1,7 @@
 # rename Plex offline to Traktor import ...
 # C:\Users\internet\AppData\Local\Plexamp\Plexamp\Offline
 #for i in `find . -type f \( ! -iname "*.flac" ! -iname "*.mp3" ! -iname "*.ogg"  \) `
+# cd /cygdrive/c/Users/internet/AppData/Local/Plexamp/Plexamp/Offline
 for i in `find . `
 do
 export FileName="$i"
