cfcfc0d3af6b80d948a76908e4ebfc11606eeead
Robert McCurdy <freeload101@users.noreply.github.com>
2024-01-31 09:36:00 -0500

    What a mess lol

--- Bash/PWS.sh
+++ Bash/PWS.sh
@@ -1,43 +1,46 @@
 PATH=/home/internet/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
- 
-while true; do 
+
+while true; do
 export SavePath="/rmccurdy/stuff/PWS/"
 
+
+
 function GetJson(){
-	date_YMD=$(date "+[%Y-%m-%d %H:%M:%S%z (%Z)]")
-	date_epoch=$(date "+%s")
-	local SiteName=${1} 
-	local StationID=${2}
-	VarJSON=$(/usr/bin/curl -s "https://api.weather.com/v2/pws/observations/current?stationId=${StationID}&format=json&units=e&apiKey=XXXXXXXXXXXXXXXXXXXXXXXXXX"   )
-	#echo "+ GetJson SiteName:${SiteName} StationID: ${StationID}"
-	echo $VarJSON >> "${SavePath}/${SiteName}_${StationID}.txt"
-	 
-	# precipRate refers to the instantaneous rate of precipitation, such as the rate of rainfall measured in inches per hour at the current moment.
-	VarprecipRate=$(echo "${VarJSON}" | grep -oP '(?<=precipRate":)(\d+.\d+|\d+)')
+        date_YMD=$(date "+%Y-%m-%d %H:%M:%S")
+        date_YMD_HR=$(date)
+        date_epoch=$(date "+%s")
+        local SiteName=${1}
+        local StationID=${2}
+        VarJSON=$(/usr/bin/curl -s "https://api.weather.com/v2/pws/observations/current?stationId=${StationID}&format=json&units=e&apiKey=53b86ecf7b4047a5b86ecf7b4027a506"   )
+        #echo "+ GetJson SiteName:${SiteName} StationID: ${StationID}"
+        echo $VarJSON >> "${SavePath}/${SiteName}_${StationID}.txt"
 
-	# precipTotal refers to the accumulated precipitation for a given time period, such as the total rainfall measured in inches over the past hour.
-	VarprecipTotal=$(echo "${VarJSON}" | grep -oP '(?<=precipTotal":)(\d+.\d+|\d+)')
+        # precipRate refers to the instantaneous rate of precipitation, such as the rate of rainfall measured in inches per hour at the current moment.
+        VarprecipRate=$(echo "${VarJSON}" | grep -oP '(?<=precipRate":)(\d+.\d+|\d+)')
 
-	echo "\"${date_YMD}\",${SiteName},${StationID},${VarprecipRate},${VarprecipTotal}"
+        # precipTotal refers to the accumulated precipitation for a given time period, such as the total rainfall measured in inches over the past hour.
+        VarprecipTotal=$(echo "${VarJSON}" | grep -oP '(?<=precipTotal":)(\d+.\d+|\d+)')
+
+        echo "\"${date_YMD}\",${SiteName},${StationID},${VarprecipRate},${VarprecipTotal}"
 }
 
 function GetJson_ambientweather(){
-	date_YMD=$(date "+[%Y-%m-%d %H:%M:%S%z (%Z)]")
-	date_epoch=$(date "+%s")
+        date_YMD=$(date "+%Y-%m-%d %H:%M:%S")
+        date_epoch=$(date "+%s")
 
-	local SiteName=${1} 
-	local StationID=${2}
-	# device ID in url NOT MAC .. API is complicated ... 
-	VarJSON=$(/usr/bin/curl -s "https://lightning.ambientweather.net/devices?public.slug=${StationID}" )
-	
-	echo $VarJSON >> "${SavePath}/${SiteName}_${StationID}.txt"
-	# eventrainin AKA precipRate ?
-	VarprecipRate=$(echo "${VarJSON}" |  grep -oP '(?<=eventrainin":)(\d+.\d+|\d+)')
-	
-	# dailyrainin AKA precipTotal ?
-	VarprecipTotal=$(echo "${VarJSON}" | grep -oP '(?<=dailyrainin":)(\d+.\d+|\d+)')
-	
-	echo "\"${date_YMD}\",${SiteName},${StationID},${VarprecipRate},${VarprecipTotal}"
+        local SiteName=${1}
+        local StationID=${2}
+        # device ID in url NOT MAC .. API is complicated ...
+        VarJSON=$(/usr/bin/curl -s "https://lightning.ambientweather.net/devices?public.slug=${StationID}" )
+
+        echo $VarJSON >> "${SavePath}/${SiteName}_${StationID}.txt"
+        # eventrainin AKA precipRate ?
+        VarprecipRate=$(echo "${VarJSON}" |  grep -oP '(?<=eventrainin":)(\d+.\d+|\d+)')
+
+        # dailyrainin AKA precipTotal ?
+        VarprecipTotal=$(echo "${VarJSON}" | grep -oP '(?<=dailyrainin":)(\d+.\d+|\d+)')
+
+        echo "\"${date_YMD}\",${SiteName},${StationID},${VarprecipRate},${VarprecipTotal}"
 }
 
 function TODO(){
@@ -61,11 +64,33 @@ GetJson "Haw Creek " "KGACUMMI349" >> "${SavePath}/current.txt"
 
 GetJson "Matt Park" "KGACUMMI297" >> "${SavePath}/current.txt"
 
-GetJson "Mt. Adams" "KGAALPHA129" >> "${SavePath}/current.txt" 
+GetJson "Mt. Adams" "KGAALPHA129" >> "${SavePath}/current.txt"
 
 
 
-cat "${SavePath}/current.txt"  >> "${SavePath}/history.txt" 
+cat "${SavePath}/current.txt"  >> "${SavePath}/history.txt"
+
+
+
+## HTML hell
+
+echo '<!DOCTYPE html><html lang="en"><head> <meta content='width=device-width, initial-scale=1' name='viewport'/> <meta charset="UTF-8"> <title>RAMBO Rain Totals</title> <link rel="icon" type="image/x-icon" href="./icons8-rain-100.png"> <style> .heading {font-family:Arial, sans-serif;font-size:14px;} .tg {border-collapse:collapse;border-spacing:0;} .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px; overflow:hidden;padding:3px 5px;word-break:normal;text-align:left} .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px; font-weight:bold;overflow:hidden;padding:5px 5px;word-break:normal;text-align:left} </style></head><body><span class="heading">'"${date_YMD_HR}"'</span><table class="tg"> <thead> <tr> <th>Gauge</th> <th>Rate (in/hr)</th> <th>Daily Total (in)</th> </tr> </thead> <tbody>' > /rmccurdy/stuff/PWS/PWS.html
+
+while IFS=',' read -r timestamp location code value1 value2
+do
+        echo '<tr>' >> /rmccurdy/stuff/PWS/PWS.html
+
+        if [ ${#code} -gt 20 ]
+        then
+                echo '<td><a href="https://ambientweather.net/dashboard/'"${code}"'" target="_blank" rel="noopener noreferrer">'"${location}"'</a></td>' >> /rmccurdy/stuff/PWS/PWS.html
+        else
+                echo '<td><a href="https://www.wunderground.com/dashboard/pws/'"${code}"'" target="_blank" rel="noopener noreferrer">'"${location}"'</a></td>' >> /rmccurdy/stuff/PWS/PWS.html
+        fi
+
+        echo '<td>'"${value1}"'</td>' >> /rmccurdy/stuff/PWS/PWS.html
+        echo '<td>'"${value2}"'</td>' >> /rmccurdy/stuff/PWS/PWS.html
+done < /rmccurdy/stuff/PWS/current.txt
+
+echo '</tbody></table></body></html>' >> /rmccurdy/stuff/PWS/PWS.html
 echo Sleeping 900 Seconds
 sleep 900
-done
