2e80efb17441fb92c6a89abadc74ba51c694ff0a
Robert McCurdy <freeload101@users.noreply.github.com>
2024-05-28 13:55:58 -0400

    Added Chrome Ext Scrape

--- Windows_Powershell_ps/GPO_CHROME_GTFO.ps1
+++ Windows_Powershell_ps/GPO_CHROME_GTFO.ps1
@@ -3,3 +3,15 @@ set-Variable -Name ErrorActionPreference -Value SilentlyContinue
 $Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command "Remove-Item -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Recurse -Force"'
 $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10) -RepetitionDuration  
 Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "RemoveChromePolicy" -Description "Removes Chrome policy registry key every 10 minutes indefinitely."
+
+
+
+Get-ChildItem -Path C:\Users\internet -Recurse -Directory -Filter "Extensions" | ForEach-Object {
+    Get-ChildItem -Path $_.FullName
+} | Select-Object -Unique -Property Name | ForEach-Object { 
+Write-Output $_.Name | Out-String
+$ID= $_.Name | Out-String
+Start-Process "C:\Users\internet\IronPortable64\IronPortable.exe" -ArgumentList "https://chromewebstore.google.com/search/$ID" 
+}
+
+
