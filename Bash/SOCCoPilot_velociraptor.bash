a8781fc93120d1ed4b0cb06e3542eabcd3d90c98
Robert McCurdy <freeload101@users.noreply.github.com>
2024-07-10 16:24:55 -0400

    Create SOCCoPilot_velociraptor.bash

---
+++ Bash/SOCCoPilot_velociraptor.bash
@@ -0,0 +1,13 @@
+export VAR_GITHUB=`curl -s https://api.github.com/repos/Velocidex/velociraptor/releases | grep -E "(.*download.*linux.*64)" | head -n 1 | awk '{print $2}'| sed -re 's/.*\"(.*).*\"/\1/g'`
+ 
+
+wget -q -O velociraptor.bin ${VAR_GITHUB}
+
+chmod 777 velociraptor.bin
+./velociraptor.bin config generate -i
+sleep 5
+./velociraptor.bin --config server.config.yaml debian server --binary velociraptor.bin --output velociraptor.deb
+sleep 5
+dpkg -i velociraptor.deb
+sleep 5
+systemctl status velociraptor_server
