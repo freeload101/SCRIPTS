647ce362f1dbfdceeceda74c08bd1ec4f644c17d
Robert McCurdy <freeload101@users.noreply.github.com>
2024-07-11 15:18:44 -0400

    Update SOCCoPilot_velociraptor.bash

--- Bash/SOCCoPilot_velociraptor.bash
+++ Bash/SOCCoPilot_velociraptor.bash
@@ -1,16 +1,26 @@
 apt update
 apt install curl -y
 
-export VAR_GITHUB=`curl -s https://api.github.com/repos/Velocidex/velociraptor/releases | grep -E "(.*download.*linux.*64)" | head -n 1 | awk '{print $2}'| sed -re 's/.*\"(.*).*\"/\1/g'`
- 
+export   VAR_GITHUB_LINUX=`curl -s https://api.github.com/repos/Velocidex/velociraptor/releases/latest | grep -E "(.*download.*linux.*amd64\")" | tail -n 1 | awk '{print $2}'| sed -re 's/.*\"(.*).*\"/\1/g'`
+export VAR_GITHUB_WINDOWS=`curl -s https://api.github.com/repos/Velocidex/velociraptor/releases/latest | grep -E "(.*download.*velociraptor-.*-windows-amd64\.msi\")" | tail -n 1 | awk '{print $2}'| sed -re 's/.*\"(.*).*\"/\1/g'`
+echo '[+] Downloading latests velociraptor Linux Binary'
+wget -q -O velociraptor.bin ${VAR_GITHUB_LINUX}
+echo '[+] Downloading latests velociraptor Windows Binary'
+wget -q -O velociraptor_ORIG.msi ${VAR_GITHUB_WINDOW}
 
-wget -q -O velociraptor.bin ${VAR_GITHUB}
-
+echo '[+] Running velociraptor config generate'
+echo '[+] I use port 80 insted of 8000 for reasons ...'
 chmod 777 velociraptor.bin
 ./velociraptor.bin config generate -i
-sleep 5
+sleep 1
+
+echo '[+] Creating Deb package'
 ./velociraptor.bin --config server.config.yaml debian server --binary velociraptor.bin --output velociraptor.deb
-sleep 5
+
+echo '[+] Installing Deb package'
 dpkg -i velociraptor.deb
 sleep 5
 systemctl status velociraptor_server
+
+echo '[+] Repacking MSI with client.config.yaml'
+./velociraptor.bin config repack --msi velociraptor_ORIG.msi client.config.yaml velociraptor_REPACKED.msi
