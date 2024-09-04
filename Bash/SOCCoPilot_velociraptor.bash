d68bc7e1986ec57c6cfad8b792906e68568c73a8
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-04 19:30:59 -0400

    Update SOCCoPilot_velociraptor.bash

--- Bash/SOCCoPilot_velociraptor.bash
+++ Bash/SOCCoPilot_velociraptor.bash
@@ -4,9 +4,9 @@ apt install curl -y
 export   VAR_GITHUB_LINUX=`curl -s https://api.github.com/repos/Velocidex/velociraptor/releases/latest | grep -E "(.*download.*linux.*amd64\")" | tail -n 1 | awk '{print $2}'| sed -re 's/.*\"(.*).*\"/\1/g'`
 export VAR_GITHUB_WINDOWS=`curl -s https://api.github.com/repos/Velocidex/velociraptor/releases/latest | grep -E "(.*download.*velociraptor-.*-windows-amd64\.msi\")" | tail -n 1 | awk '{print $2}'| sed -re 's/.*\"(.*).*\"/\1/g'`
 echo '[+] Downloading latests velociraptor Linux Binary'
-#wget -q -O velociraptor.bin ${VAR_GITHUB_LINUX}
+wget -q -O velociraptor.bin ${VAR_GITHUB_LINUX}
 echo '[+] Downloading latests velociraptor Windows Binary'
-#wget -q -O velociraptor_ORIG.msi "${VAR_GITHUB_WINDOWS}"
+wget -q -O velociraptor_ORIG.msi "${VAR_GITHUB_WINDOWS}"
 
 echo '[+] Running velociraptor config generate'
 echo '[+] I use port 80 insted of 8000 for reasons ...'
