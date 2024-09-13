ddc4c9a0fa5ab07056a43e7f5ebf8e658a8cb517
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-13 10:43:38 -0400

    Update SOCFortress_CoPilot_Wazuh.sh

--- Bash/SOCFortress_CoPilot_Wazuh.sh
+++ Bash/SOCFortress_CoPilot_Wazuh.sh
@@ -7,16 +7,18 @@ echo '[+] Installing Docker'
 #curl -sSL https://get.docker.com/ | sh
 #systemctl restart  docker
 
-echo '[+] Installing docker-compose'
-curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+#echo '[+] Installing docker-compose'
+#curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
 
 echo '[+] Download Wazyh https://github.com/wazuh/wazuh-docker.git -b v4.8.2'
 git clone https://github.com/wazuh/wazuh-docker.git -b v4.8.2
 
 echo '[+] Setting up Wazuh certs'
+echo '[+] NOTE: using docker-compose per docs but docker compose '
+
 cd wazuh-docker/single-node/
-docker-compose -f generate-indexer-certs.yml run --rm generator
+docker compose -f generate-indexer-certs.yml run --rm generator
 
 
 
