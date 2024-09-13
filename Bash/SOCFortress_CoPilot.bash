b842ccf6dc4fbc2f37f966a75f6249bb70bc8d79
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-13 09:12:38 -0400

    Update SOCFortress_CoPilot.bash

--- Bash/SOCFortress_CoPilot.bash
+++ Bash/SOCFortress_CoPilot.bash
@@ -10,8 +10,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
+ 
  
 echo '[+] Installing Docker'
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
