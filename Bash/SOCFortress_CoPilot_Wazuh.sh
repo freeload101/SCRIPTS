266d2ea2bb093ab41e7c0f6be8157628abb3e77b
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-13 18:52:55 -0400

    Update SOCFortress_CoPilot_Wazuh.sh

--- Bash/SOCFortress_CoPilot_Wazuh.sh
+++ Bash/SOCFortress_CoPilot_Wazuh.sh
@@ -3,8 +3,6 @@
 
 #wsl --update
 #wsl --set-default-version 2
-	
-
 
 #wsl --install 
 
@@ -21,51 +19,25 @@ apt install curl -y
  
 curl -sSL https://get.docker.com/ | sh
  
- 
 systemctl start docker
 systemctl enable docker
-
 
 curl -L "https://github.com/docker/compose/releases/download/1.28.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
-
-
-
 echo '[+] It is recommended to configure the Docker host preferences to give at least 6GB of memory. setting vm.max_map_count=262144'
 echo vm.max_map_count=262144 >> /etc/sysctl.conf
 sysctl -w vm.max_map_count=262144
 
-
 git clone https://github.com/wazuh/wazuh-docker.git -b v4.2.6 --depth=1
-
 cd ./wazuh-docker
 
 docker-compose -f generate-opendistro-certs.yml run --rm generator
 
-
 bash ./production_cluster/kibana_ssl/generate-self-signed-cert.sh
-
 
 bash ./production_cluster/nginx/ssl/generate-self-signed-cert.sh
 
-
 docker-compose -f production-cluster.yml up -d
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-s
