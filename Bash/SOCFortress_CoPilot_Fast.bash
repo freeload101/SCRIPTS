233e81f5e1da1b92e1fb57931d4c7a07e923a7fc
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-14 00:20:32 -0400

    Update SOCFortress_CoPilot_Fast.bash

--- Bash/SOCFortress_CoPilot_Fast.bash
+++ Bash/SOCFortress_CoPilot_Fast.bash
@@ -54,44 +54,6 @@ echo '[+] Starting Wazuh production-cluster'
 docker-compose -f production-cluster.yml up -d
 
 
-##############################################################  CoPilot
-echo '[+] config docker DNS to default DNS'
-export ROUTE=`ip route show default | awk '{print $3}' | head`
-echo $ROUTE
-echo "{\"dns\":[\"`echo $ROUTE`\"],\"log-driver\":\"json-file\",\"log-opts\":{\"max-size\":\"10m\",\"max-file\":\"3\"},\"mtu\": 1450}" >  /etc/docker/daemon.json
-echo '[+] install CoPilot'
-mkdir /opt
-mkdir /opt/CoPilot
-cd /opt/CoPilot
-
-wget https://raw.githubusercontent.com/socfortress/CoPilot/v0.0.8/docker-compose.yml
-mkdir data
-
-echo '[+] Create the .env file based on the .env.example'
-wget 'https://raw.githubusercontent.com/socfortress/CoPilot/main/.env.example'
-
-echo '[+] get internet IP and update .env with it'
-export INTERNETIP=`ip route get 1.1.1.1  | awk '{print $7}' | head -n 1`
-sed  -re "s/(ALERT_FORWARDING_IP=)0.0.0.0/\1$INTERNETIP/g"  .env.example > .env
-
-echo '[+] Restarting docker'
-systemctl daemon-reload
-systemctl restart docker
-
-echo '[+] Setting port to 4433 from 433 and 80 to 800 because Wazuh uses 443 and 80'
-
-sed -re 's/443:443/4433:443/g' -re 's/80:80/800:800/g' /opt/CoPilot/docker-compose.yml -i.bak
-
-
-echo '[+] Run Copilot'
-docker compose up -d
-
-echo '[+] Waiting for CoPilot to start to show password'
-sleep  10
-docker logs "$(docker ps --filter ancestor=ghcr.io/socfortress/copilot-backend:latest --format "{{.ID}}")" 2>&1 | grep "Admin user password" | sed -re 's/.*plain=.(.*).$/\1/g' > PASSWORD
-sleep  10
-docker logs "$(docker ps --filter ancestor=ghcr.io/socfortress/copilot-backend:latest --format "{{.ID}}")" 2>&1 | grep "Admin user password" | sed -re 's/.*plain=.(.*).$/\1/g' >> PASSWORD
- 
  
  
 ##############################################################  Velociraptor
@@ -143,20 +105,61 @@ echo '[+] get internet IP and update api.config.yaml with it'
 export INTERNETIP=`ip route get 1.1.1.1  | awk '{print $7}' | head -n 1`
 sed  -re "s/localhost:8001/$INTERNETIP:8001/g"  api.config.yaml -i.bak
 cp -R api.config.yaml /mnt/c/delete/
+cp -R api.config.yaml /tmp/
 cp -R velociraptor_REPACKED.msi /mnt/c/delete/
 
  
 
 
 
+##############################################################  CoPilot
+echo '[+] config docker DNS to default DNS'
+export ROUTE=`ip route show default | awk '{print $3}' | head`
+echo $ROUTE
+echo "{\"dns\":[\"`echo $ROUTE`\"],\"log-driver\":\"json-file\",\"log-opts\":{\"max-size\":\"10m\",\"max-file\":\"3\"},\"mtu\": 1450}" >  /etc/docker/daemon.json
+echo '[+] install CoPilot'
+mkdir /opt
+mkdir /opt/CoPilot
+cd /opt/CoPilot
 
-'[+] Should be running on https://localhost:8889 with login root and password password ! '
+wget https://raw.githubusercontent.com/socfortress/CoPilot/v0.0.8/docker-compose.yml
+mkdir data
+
+echo '[+] Create the .env file based on the .env.example'
+wget 'https://raw.githubusercontent.com/socfortress/CoPilot/main/.env.example'
+
+echo '[+] get internet IP and update .env with it'
+export INTERNETIP=`ip route get 1.1.1.1  | awk '{print $7}' | head -n 1`
+sed -re "s/(ALERT_FORWARDING_IP=)0.0.0.0/\1$INTERNETIP/g"  -re "s/WAZUH_INDEXER_URL=.*/WAZUH_INDEXER_URL=https:\/\/$INTERNETIP:9200/g" -re "s/WAZUH_INDEXER_PASSWORD=.*/WAZUH_INDEXER_PASSWORD=SecretPassword/g"   -re "s/WAZUH_MANAGER_URL=.*/WAZUH_MANAGER_URL=https:\/\/$INTERNETIP:55000/g" -re "s/WAZUH_MANAGER_USERNAME=.*/WAZUH_MANAGER_USERNAME=acme-user/g"  -re "s/WAZUH_MANAGER_PASSWORD=.*/WAZUH_MANAGER_PASSWORD=MyS3cr37P450r.*-/g" -re "s/VELOCIRAPTOR_URL=.*/VELOCIRAPTOR_URL=https:\/\/$INTERNETIP:8001/g" -re "s/VELOCIRAPTOR_API_KEY_PATH=.*/VELOCIRAPTOR_API_KEY_PATH=\/tmp\/api.config.yaml/g" .env.example > .env
+
+
+echo '[+] Restarting docker'
+systemctl daemon-reload
+systemctl restart docker
+
+echo '[+] Setting port to 4433 from 433 and 80 to 800 because Wazuh uses 443 and 80'
+
+sed -re 's/443:443/4433:443/g' -re 's/80:80/800:80/g' /opt/CoPilot/docker-compose.yml -i.bak
+
+
+echo '[+] Run Copilot'
+docker compose up -d
+
+echo '[+] Waiting for CoPilot to start to show password'
+sleep  10
+docker logs "$(docker ps --filter ancestor=ghcr.io/socfortress/copilot-backend:latest --format "{{.ID}}")" 2>&1 | grep "Admin user password" | sed -re 's/.*plain=.(.*).$/\1/g' > PASSWORD
+sleep  10
+docker logs "$(docker ps --filter ancestor=ghcr.io/socfortress/copilot-backend:latest --format "{{.ID}}")" 2>&1 | grep "Admin user password" | sed -re 's/.*plain=.(.*).$/\1/g' >> PASSWORD
+ 
+ 
+ 
    
 
 ##############################################################  END
 export INTERNETIP=`ip route get 1.1.1.1  | awk '{print $7}' | head -n 1`
-echo "[+] Wazuh Web UI: https://$INTERNETIP admin:SecretPassword Wazuh API for SOCFortress CoPilot"
-echo "[+] Wazuh API:    https://$INTERNETIP:55000  acme-user:MyS3cr37P450r.*- Wazuh API for SOCFortress CoPilot"
+echo "[+] Wazuh Web UI: https://$INTERNETIP admin:SecretPassword "
+echo "[+] Wazuh Indexer API: https://172.29.137.13:9200 admin:SecretPassword"
+echo "[+] Wazuh Manager API: https://$INTERNETIP:55000  acme-user:MyS3cr37P450r.*- Wazuh API for SOCFortress CoPilot"
 echo "[+] Velociraptor: https://$INTERNETIP:8889  root:password  SOCFortress CoPilot: port 8001 check api.config.yaml"
 echo "[+] Velociraptor API: check api.config.yaml port 8001"
 echo "[+] SOCFortress CoPilot: https://$INTERNETIP:4433 admin:`cat /opt/CoPilot/PASSWORD` and https://$INTERNETIP:800 "
