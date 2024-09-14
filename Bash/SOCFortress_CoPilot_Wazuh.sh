e659059387bdf01b9116e5a56142df442045476b
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-13 20:15:04 -0400

    Update SOCFortress_CoPilot_Wazuh.sh

--- Bash/SOCFortress_CoPilot_Wazuh.sh
+++ Bash/SOCFortress_CoPilot_Wazuh.sh
@@ -44,3 +44,5 @@ bash ./production_cluster/nginx/ssl/generate-self-signed-cert.sh
 
 docker-compose -f production-cluster.yml up -d
 echo '[+] https://localhost and https://localhost:55000 for SOC Fortress CoPilot'
+# docker compose down --remove-orphans
+
