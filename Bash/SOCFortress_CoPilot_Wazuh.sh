0648558e5372d0cce42d667952c364d5aa03e42f
Robert McCurdy <freeload101@users.noreply.github.com>
2024-09-13 20:38:53 -0400

    Update SOCFortress_CoPilot_Wazuh.sh

--- Bash/SOCFortress_CoPilot_Wazuh.sh
+++ Bash/SOCFortress_CoPilot_Wazuh.sh
@@ -46,6 +46,6 @@ bash ./production_cluster/nginx/ssl/generate-self-signed-cert.sh
 
 
 docker-compose -f production-cluster.yml up -d
-echo '[+] https://localhost for WEBUI login:password admin:SecretPassword and https://$INTERNETIP:55000 for SOC Fortress CoPilot'
+echo '[+] https://localhost for WEBUI login:password admin:SecretPassword and https://$INTERNETIP:55000  with login wazuh-wui:wazuh-wui for SOC Fortress CoPilot'
 # docker compose down --remove-orphans
 
