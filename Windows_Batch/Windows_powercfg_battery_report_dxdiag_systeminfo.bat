bfd8e5ee92c8d9c00a69782228ab2eb80ae12bbe
Robert McCurdy <freeload101@users.noreply.github.com>
2025-01-04 09:34:24 -0500

    Update Windows_powercfg_battery_report_dxdiag_systeminfo.bat

--- Windows_Batch/Windows_powercfg_battery_report_dxdiag_systeminfo.bat
+++ Windows_Batch/Windows_powercfg_battery_report_dxdiag_systeminfo.bat
@@ -5,6 +5,6 @@ start /wait cmd /c "msinfo32 /report "%temp%\msinfo32.txt""
 type  "%temp%\msinfo32.txt" >>   "%temp%\dxdiag.txt"
 powercfg /batteryreport /output "%temp%\battery-report.html"
 
-start  "%temp%\battery-report.html"
-start "%temp%\dxdiag.txt"
+start  "" "%temp%\battery-report.html"
+start "" "%temp%\dxdiag.txt"
 
