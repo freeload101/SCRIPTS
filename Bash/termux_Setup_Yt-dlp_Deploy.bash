04a02979ceb49ec073e71f794cedca0f6872829b
Robert McCurdy <freeload101@users.noreply.github.com>
2025-06-29 13:34:23 -0400

    Update termux_Setup_Yt-dlp_Deploy.bash

--- Bash/termux_Setup_Yt-dlp_Deploy.bash
+++ Bash/termux_Setup_Yt-dlp_Deploy.bash
@@ -1,5 +1,10 @@
 # I keep forgetting termux setup
-termux-setup-storage
-pkg install yt-dlp -y
+termux-change-repo
+pkg update && pkg upgrade -y  
+pkg install proot-distro -y
+termux-setup-storage  
+
 echo "cd ~/storage/downloads" >> ~/.bashrc
 source ~/.bashrc
+
+pkg install yt-dlp -y
