de2dfa6c64d2dc6da8ddaf81f53be8de902a4f70
Robert McCurdy <freeload101@users.noreply.github.com>
2025-09-11 00:58:44 -0400

    Fixed it again ...

--- Windows_Powershell_ps/Yt-dlp-ffmpeg-aria2c-updater-downloader.ps1
+++ Windows_Powershell_ps/Yt-dlp-ffmpeg-aria2c-updater-downloader.ps1
@@ -4,15 +4,9 @@ if (-not (Test-Path "C:\Windows\SysWOW64\msvcr100.dll") -and -not (Test-Path "C:
     (New-Object Net.WebClient).DownloadFile("https://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe", ".\vcredist_x86.exe")
     Start-Process ".\vcredist_x86.exe" -ArgumentList "/q", "/norestart" -Wait -WindowStyle Hidden
 }
-
-# Open notepad for URL list
+  
+"Paste URLS into this and save it" | Out-File -FilePath "list.txt" -Encoding UTF8
 Start-Process -FilePath "cmd" -ArgumentList "/c", "notepad list.txt" -Wait
-
-# Download wget if missing
-if (-not (Test-Path ".\wget.exe")) {
-    Write-Host "$(Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt') INFO: Downloading wget.exe"
-    (New-Object Net.WebClient).DownloadFile('https://eternallybored.org/misc/wget/1.20.3/64/wget.exe', '.\wget.exe')
-}
 
 # Download latest aria2c if missing
 if (-not (Test-Path ".\aria2c.exe")) {
@@ -31,12 +25,43 @@ if (-not (Test-Path ".\aria2c.exe")) {
 
 # Download or update yt-dlp
 if (Test-Path ".\yt-dlp.exe") {
-    .\yt-dlp.exe -U
+ .\yt-dlp.exe -U
 } else {
     Write-Host "$(Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt') INFO: Downloading yt-dlp.exe"
     (New-Object Net.WebClient).DownloadFile('https://github.com/yt-dlp/yt-dlp/releases/download/2025.09.05/yt-dlp.exe', '.\yt-dlp.exe')
 }
+ 
+if (-not (Test-Path ".\ffmpeg.exe")) {
+    function downloadFile($url, $targetFile) {
+        $uri = New-Object "System.Uri" "$url"
+        $request = [System.Net.HttpWebRequest]::Create($uri)
+        $request.set_Timeout(15000)
+        $response = $request.GetResponse()
+        $responseStream = $response.GetResponseStream()
+        $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
+        $buffer = new-object byte[] 10KB
+        $count = $responseStream.Read($buffer,0,$buffer.length)
+        while ($count -gt 0) {
+            $targetStream.Write($buffer, 0, $count)
+            $count = $responseStream.Read($buffer,0,$buffer.length)
+        }
+        $targetStream.Flush()
+        $targetStream.Close()
+        $targetStream.Dispose()
+        $responseStream.Dispose()
+    }
 
+    Write-Host "$(Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt') INFO: Downloading ffmpeg.exe"
+    $zip = ".\temp.zip"
+    $extract = ".\temp"
+    downloadFile "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip" $zip
+    Expand-Archive -Path $zip -DestinationPath $extract -Force
+    $ffmpeg = Get-ChildItem -Path $extract -Name "ffmpeg.exe" -Recurse | Select-Object -First 1
+    Move-Item -Path (Join-Path $extract $ffmpeg) -Destination ".\ffmpeg.exe" -Force
+    Remove-Item -Path $zip, $extract -Recurse -Force
+}
+
+ 
 # Process URLs
 $timeout = if ($env:WAITTIME) { [int]$env:WAITTIME } else { 30 }
 Get-Content "list.txt" | ForEach-Object {
@@ -49,6 +74,6 @@ Get-Content "list.txt" | ForEach-Object {
 
     if (-not (Get-ChildItem ".\downloads" -Name "*$uuid*" -ErrorAction SilentlyContinue)) {
         Write-Host "$(Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt') ERROR: No files found for `"$_`", trying legacy mode"
-        Start-Process cmd -ArgumentList "/c", "yt-dlp.exe -w --no-continue --merge-output-format mkv --ffmpeg-location .\ -o `"$template`" `"$_`" & pause" -WindowStyle Normal
+        Start-Process cmd -ArgumentList "/c", "yt-dlp.exe -w --no-continue  --ffmpeg-location .\ -o `"$template`" `"$_`" & pause" -WindowStyle Normal
     }
 }
