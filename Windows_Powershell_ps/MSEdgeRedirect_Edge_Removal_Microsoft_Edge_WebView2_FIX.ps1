d75dca9973c6f6b51a906c576aeca91a4d18bf63
Robert McCurdy <freeload101@users.noreply.github.com>
2026-02-02 09:45:42 -0500

    Enhance script to install WebView2 and app from Store
    Added functionality to download and install WebView2 Runtime silently, and included a command to install an application from the Microsoft Store.

--- Windows_Powershell_ps/MSEdgeRedirect_Edge_Removal_Microsoft_Edge_WebView2_FIX.ps1
+++ Windows_Powershell_ps/MSEdgeRedirect_Edge_Removal_Microsoft_Edge_WebView2_FIX.ps1
@@ -1,26 +1,21 @@
 # this is WIP BUT NO REBOOT NEEDED AT LEAST ON WINDOWS 11
 # I don't know what of this fixed it but  I don't have time to test 
 
-Write-Output "Downloading MSEdgeRedirect"
-$downloadUri = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/rcmaehl/MSEdgeRedirect/releases/latest").assets | Where-Object name -like *.exe ).browser_download_url
-Invoke-WebRequest -Uri "$downloadUri" -OutFile "c:\windows\temp\MSEdgeRedirect.exe"
-# run this ... "c:\windows\temp\MSEdgeRedirect.exe" /uninstall
-
-Write-Output "downloading https://github.com/AveYo/fox/raw/main/Edge_Removal.bat"
-Invoke-WebRequest -Uri "https://github.com/AveYo/fox/raw/main/Edge_Removal.bat" -OutFile "c:\windows\temp\Edge_Removal.bat" 
-
-
-# idk ... RUN "c:\windows\temp\Edge_Removal.bat" edge 
-# I have no idea ...
-#  EDGE REMOVED!  -GET-ANOTHER-BROWSER? ENTER: firefox  -REINSTALL? ENTER: edge / webview / xsocial 
-
 # fixes play store
 Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"};wsreset -i;Get-AppxPackage -allusers Microsoft.MicrosoftEdge* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 
 # allows install of edge or webview IDK
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\EdgeUpdate" -Name "Install{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}" -Value 1 -Type DWord -Force
+ 
+# Download and silently install WebView2 Runtime
+$url = "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/6d376ab4-4a07-4679-8918-e0dc3c0735c8/MicrosoftEdgeWebView2RuntimeInstallerX64.exe"
+$installer = "$env:TEMP\MicrosoftEdgeWebView2RuntimeInstallerX64.exe"
 
-# this reinstalls webviewe 
-# download install msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/6d376ab4-4a07-4679-8918-e0dc3c0735c8/MicrosoftEdgeWebView2RuntimeInstallerX64.exe 
+# Download installer
+Invoke-WebRequest -Uri $url -OutFile $installer
 
+# Silent install with arguments
+Start-Process -FilePath $installer -ArgumentList "/silent /install" -Wait -NoNewWindow
 
+# open play store and install something random
+winget install "LinkedIn" --source msstore
