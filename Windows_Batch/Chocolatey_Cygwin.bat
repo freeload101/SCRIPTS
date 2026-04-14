ac31ffc79ff0a505df267d7fec01e43fa733055b
Robert McCurdy <freeload101@users.noreply.github.com>
2026-04-13 21:28:40 -0700

    Fixed bebloat path ?

--- Windows_Batch/Chocolatey_Cygwin.bat
+++ Windows_Batch/Chocolatey_Cygwin.bat
@@ -135,7 +135,7 @@ CALL :theEnd
 :DEBLOAT
 echo [+] Downloading/Installing Win10Hardening_Debloat.ps1
 ::download install.ps1
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://github.com/freeload101/SCRIPTS/raw/master/Windows_Powershell_ps/Win10_11_Hardening_Debloat.ps1','%DIR%Win10Hardening_Debloat.ps1'))"
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://github.com/freeload101/SCRIPTS/raw/refs/heads/master/Windows_Powershell_ps/Win10_11_Hardening_Debloat.ps1','%DIR%Win10Hardening_Debloat.ps1'))"
 ::run installer
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%Win10Hardening_Debloat.ps1' %*"
 EXIT /B %ERRORLEVEL%
